7905496515a8c1934c5d00865db07cfd8c4171ea
Kevin Cheng <dcvsling@gmail.com>
2024-05-13 01:42:49 +0800

    move translate url to app init

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,4 +1,4 @@
-import { ApplicationConfig, importProvidersFrom, makeEnvironmentProviders } from '@angular/core';
+import { ApplicationConfig, ENVIRONMENT_INITIALIZER, importProvidersFrom, makeEnvironmentProviders } from '@angular/core';
 import { provideRouter, withEnabledBlockingInitialNavigation } from '@angular/router';
 
 import { routes } from './app.routes';
@@ -8,6 +8,8 @@ import { provideMsalAuth } from '../auth';
 import { IMAGE_CONFIG } from '@angular/common';
 import { provideLocalStorageDataProvider } from '../storage';
 import { VIDEO_LIST_ACEESSOR_PROVIDER } from './services/list.local';
+
+
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -23,8 +25,23 @@ export const appConfig: ApplicationConfig = {
       ? [withEnabledBlockingInitialNavigation()]
       : []),
     ),
+    {
+      provide: ENVIRONMENT_INITIALIZER,
+      useValue: () => {
+        const l = window.location;
+        if (l.search[1] === '/' ) {
+          var decoded = l.search.slice(1).split('&').map(function(s) {
+            return s.replace(/~and~/g, '&')
+          }).join('?');
+          window.history.replaceState(
+            null,
+            '',
+            l.pathname.slice(0, -1) + decoded + l.hash);
+        }
+      },
+      multi: true
+    },
     VIDEO_LIST_ACEESSOR_PROVIDER,
-    provideLocalStorageDataProvider('video'),
     provideLocalStorageDataProvider('video'),
     provideAnimationsAsync(),
     provideMsalAuth(),
