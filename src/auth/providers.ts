a8b821fa8689266dda8a0d8a2fb07d586950f438
Kevin Cheng <dcvsling@gmail.com>
2025-06-25 10:20:38 +0800

    fix location

--- src/auth/providers.ts
+++ src/auth/providers.ts
@@ -1,9 +1,10 @@
 import { HTTP_INTERCEPTORS, withFetch, provideHttpClient, withInterceptorsFromDi } from "@angular/common/http";
-import { makeEnvironmentProviders } from "@angular/core";
+import { makeEnvironmentProviders, PLATFORM_ID } from "@angular/core";
 import { AuthHttpInterceptor } from "./interceptor";
 import { GetAccessTokenOptions } from "./options";
 import { AuthClient } from "./auth.client";
 import { IAccessTokenProvider } from "./accessor";
+import { isPlatformBrowser } from "@angular/common";
 
 
 
@@ -17,29 +18,33 @@ export function provideAuth() {
     },
     {
       provide: GetAccessTokenOptions,
-      useValue: {
-        client_id: '99a45bd5-73e9-4439-a10d-9d00f9ae0052',
-        scope: [
-          "https://graph.microsoft.com/User.Read",
-          "https://graph.microsoft.com/Files.Read",
-          "https://graph.microsoft.com/Files.Read.All"
-        ],
-        GetAcccessTokenParemeters: {
-          grant_type: "authorization_code",
-          redirect_uri: encodeURIComponent(`${location?.origin}/auth`),
-          code_verifier: "HfdeySOBvTW4aAu3S0jRC4U-Njvhn0gaK733LJhB4Q-sXGrKP9RSNJg9FWX338WV0f28UpLQV-SROg2jXealgS4Qen53ViQjhi2T4MmULNtKYHRbRClJOzMbvheHl-uY"
-        },
-        GetCodeRequestParameters: {
-          response_type: 'code',
-          redirect_uri: encodeURIComponent(`${location?.origin}/auth`),
-          response_mode: 'query',
-          code_challenge: '778s-lN2H4vsQE45ckbqaTPVEfgcd-oY1RiGUwmOtVs',
-          code_challenge_method: 'S256'
-        },
-        RefreshAccessTokenParameters: {
-          grant_type: 'refresh_token'
-        }
-      } as GetAccessTokenOptions
+      useFactory(obj: object): GetAccessTokenOptions {
+        return {
+          client_id: '99a45bd5-73e9-4439-a10d-9d00f9ae0052',
+          scope: [
+            "https://graph.microsoft.com/User.Read",
+            "https://graph.microsoft.com/Files.Read",
+            "https://graph.microsoft.com/Files.Read.All"
+          ],
+          GetAcccessTokenParemeters: {
+            grant_type: "authorization_code",
+            redirect_uri: encodeURIComponent(`${isPlatformBrowser(obj) ? location.origin : ''}/auth`),
+            code_verifier: "HfdeySOBvTW4aAu3S0jRC4U-Njvhn0gaK733LJhB4Q-sXGrKP9RSNJg9FWX338WV0f28UpLQV-SROg2jXealgS4Qen53ViQjhi2T4MmULNtKYHRbRClJOzMbvheHl-uY"
+          },
+          GetCodeRequestParameters: {
+            response_type: 'code',
+            redirect_uri: encodeURIComponent(`${isPlatformBrowser(obj) ? location.origin : ''}/auth`),
+            response_mode: 'query',
+            code_challenge: '778s-lN2H4vsQE45ckbqaTPVEfgcd-oY1RiGUwmOtVs',
+            code_challenge_method: 'S256'
+          },
+          RefreshAccessTokenParameters: {
+            grant_type: 'refresh_token'
+          }
+        } as GetAccessTokenOptions
+      } ,
+      deps: [PLATFORM_ID]
+       
     },
     AuthClient,
     {
